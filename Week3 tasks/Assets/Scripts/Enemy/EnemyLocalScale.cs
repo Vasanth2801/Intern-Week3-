36d632685fc60388dabfe0536143988e099b30ed
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-29 11:35:53 +0530

    A* path finder for Enemy

---
+++ Week3 tasks/Assets/Scripts/Enemy/EnemyLocalScale.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyLocalScale : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
