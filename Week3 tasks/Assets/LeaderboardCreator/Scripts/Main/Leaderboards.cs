13ce0be84c73b5d0ef3f59a027161e171c744093
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-10-06 21:04:35 +0530

    Leader Board and transtions

---
+++ Week3 tasks/Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference PraticeLeaderBoard = new LeaderboardReference("99268462b7441e2028a21280548c6dcd7fc90bb1bae62bb48048465a49e37493");
+    }
+}
